4b46e34c641588cf03e74556b0ae92a363ea81e5
PATELCHIRAG4597 <124140359+PATELCHIRAG4597@users.noreply.github.com>
2023-06-05 12:03:09 +0530

    all done

--- loginPageforIPADE/ViewController.swift
+++ loginPageforIPADE/ViewController.swift
@@ -7,13 +7,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController,UITextFieldDelegate {
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var btnLogin: UIButton!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        txtEmail.delegate = self
+//                configurePlaceholderText()
+      
+        txtEmail.layer.cornerRadius = 7.0
+        txtEmail.layer.masksToBounds = true
+        txtPassword.layer.cornerRadius = 7.0
+        txtPassword.layer.masksToBounds = true
+        btnLogin.layer.cornerRadius = 7.0
+        btnLogin.layer.masksToBounds = true
     }
 
-
+//    func configurePlaceholderText() {
+//        let placeholder = "Email"
+//        let placeholderColor = UIColor.gray
+//        let textColor = UIColor.black
+//
+//        let attributes: [NSAttributedString.Key: Any] = [
+//            .foregroundColor: placeholderColor,
+//            .font: txtEmail.font ?? UIFont.systemFont(ofSize: 17)
+//        ]
+//
+//        let attributedPlaceholder = NSAttributedString(string: placeholder, attributes: attributes)
+//        txtEmail.attributedPlaceholder = attributedPlaceholder
+//        txtEmail.textColor = textColor
+//    }
+    
+    
+    @IBAction func btnLoginTapped(_ sender: Any) {
+       
+    }
 }
 
+//extension ViewController: UITextFieldDelegate {
+//    func textFieldDidBeginEditing(_ textField: UITextField) {
+//        if textField == txtEmail {
+//            if textField.text?.isEmpty ?? true {
+//                textField.attributedPlaceholder = nil
+//            }
+//        }
+//    }
+//
+//    func textFieldDidEndEditing(_ textField: UITextField) {
+//        if textField == txtEmail {
+//            if textField.text?.isEmpty ?? true {
+//                configurePlaceholderText()
+//            }
+//        }
+//    }
+//}
